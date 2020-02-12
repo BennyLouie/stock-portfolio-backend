e9266b419f5e2b4b328b72b8760e03f84ebbda5a
BennyLouie <bennylouie1412@gmail.com>
2020-02-12 15:32:08 -0500

    updated controller methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+    private
+
+    def encode_token(payload)
+        JWT.encode(payload, signing_secret, 'HS256')
+    end
+
+    def signing_secret
+        ENV["JWT_SECRET_KEY"]
+    end
+
 end
