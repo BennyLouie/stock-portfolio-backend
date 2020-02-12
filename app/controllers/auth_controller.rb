3972fa76333fbcc827327f94bb6c06f066f4f238
BennyLouie <bennylouie1412@gmail.com>
2020-02-12 15:49:40 -0500

    updated controller methods

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,2 +1,21 @@
 class AuthController < ApplicationController
+
+    def login
+        user = User.find_by(email: params[:email])
+        if user && user.authenticate(params[:password])
+            payload = { user_id: user.id }
+            token = encode_token(payload)
+            render json: { user: user, jwt: token, success: "Welcome back, #{user.first_name}"}
+        else 
+            render json: { failure: "Invalid Email or Password!"}
+        end
+    end
+
+    def auto_login
+        if session_user
+            render json: session_user
+        else 
+            render json: { error: "No User Logged In"}
+        end
+    end
 end
