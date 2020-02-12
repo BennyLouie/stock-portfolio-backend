9b0e48886381473095e528177bfab4be879feda5
BennyLouie <bennylouie1412@gmail.com>
2020-02-12 15:39:15 -0500

    added unique email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
     validates_presence_of :first_name
     validates_presence_of :last_name
     validates_presence_of :email
+
+    validates :email, uniqueness: true
     
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
